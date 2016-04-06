05e1f61974fab9f15804585b5a24d00446c34725
redboul <julien.reboul@bonitasoft.com>
2016-04-06 18:44:39 +0200

    find current page inside navigation

--- wp-ng/app/components/navigation/navigation.controller.js
+++ wp-ng/app/components/navigation/navigation.controller.js
@@ -6,14 +6,25 @@ export default class NavigationCtrl {
     this.version = $stateParams.version || versions.defaultVersion || '7.3';
     this.$stateParams = $stateParams;
     this.$state = $state;
+    if ($stateParams.page) {
+      taxonomy.forEach(taxo => this.expandPathToPage(taxo, $stateParams.page));
+    }
   }
   goTo(page) {
     this.$state.transitionTo(this.$state.current, angular.extend(this.$stateParams, { page: page.replace(/\.(md|html)$/, '') }), {
       reload: false, inherit: true, notify: false
     });
   }
-  expandPathToPage(page) {
-    return page;
+  expandPathToPage(node, page) {
+    const name = node.href && node.href.replace(/\.(md|html)$/, '');
+    if (name && name === page) {
+      node.isExpanded = true;
+    } else if (node.children) {
+      node.isExpanded = node.children.reduce((acc, curr) => {
+        return acc || this.expandPathToPage(curr, page);
+      }, false);
+    }
+    return node.isExpanded;
   }
   isPageCurrentlyDisplayed(page) {
     return page.replace(/\.(md|html)$/, '') === this.$stateParams.page;
