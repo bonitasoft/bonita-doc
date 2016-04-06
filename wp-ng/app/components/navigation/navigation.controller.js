e630063c5c995389294d08cc1df1fb92ede2b6fd
redboul <julien.reboul@bonitasoft.com>
2016-04-06 14:45:25 +0200

    add icons on navigation

--- wp-ng/app/components/navigation/navigation.controller.js
+++ wp-ng/app/components/navigation/navigation.controller.js
@@ -1,5 +1,24 @@
 export default class NavigationCtrl {
   constructor() {
-    this.data = [{title: 'test 1', data: [{title: 'subtitle 1'}, {title: 'subtitle 2'}]}, {title: 'test 2'}];
+    this.data = [{
+      title: 'test 1',
+      data: [{
+        title: 'subtitle 1',
+        data: [{
+          title: 'subsubtitle 1',
+          data: [{
+            title: 'subsubsubtitle 1'
+          }, {
+            title: 'subsubsubtitle 2'
+          }]
+        }, {
+          title: 'subsubtitle 2'
+        }]
+      }, {
+        title: 'subtitle 2'
+      }]
+    }, {
+      title: 'test 2'
+    }];
   }
 }
