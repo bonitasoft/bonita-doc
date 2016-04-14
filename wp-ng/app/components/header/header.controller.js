a0d3b44ab2a7bca31b3828fb42f84bc9377d1b5a
Julien Reboul <julien.reboul@bonitasoft.com>
2016-04-14 22:28:52 +0200

    add nath return and confirmation before going to former documentation site

--- wp-ng/app/components/header/header.controller.js
+++ wp-ng/app/components/header/header.controller.js
@@ -1,21 +1,36 @@
-import angular from 'angular';
+import _ from 'lodash';
 
 export default class HeaderController {
-  constructor(properties, $stateParams, $state, $http) {
+  constructor(properties, $stateParams, $state, $http, $window, $uibModal) {
     'ngInject';
-    this.supportedVersionList = properties.supportedVersionList.filter((item, index) => index < 5);
-    this.unsupportedVersionList = properties.supportedVersionList.filter((item, index) => index >= 5);
+    this.supportedVersionList = properties.supportedVersionList.filter((item, index) => index < 4);
+    this.unsupportedVersionList = properties.supportedVersionList.filter((item, index) => index >= 4);
     this.selectedVersion = $stateParams.version || properties.defaultVersion;
     this.$state = $state;
     this.$stateParams = $stateParams;
     this.$http = $http;
+    this.modal = $uibModal;
+    this.window = $window;
   }
   selectVersion(version) {
-    this.$state.transitionTo('main.content', angular.extend(this.$stateParams, { version: version }), {
+    this.$state.transitionTo('main.content', _.extend(this.$stateParams, { version: version }), {
       reload: false, inherit: false, notify: true
     });
   }
   search(searchRequest) {
     this.$state.go('main.content.search', {searchRequest: searchRequest, page: null});
+  }
+  goToFormerDocumentationSite(versionFragment) {
+    this.modal.open({
+      template:
+      `<div class="modal-body">
+        You will be redirected to the former documentation site
+       </div>
+       <div class="modal-footer ng-scope">
+            <button class="btn btn-primary" type="button" ng-click="$close()">OK</button>
+            <button class="btn btn-warning" type="button" ng-click="$dismiss()">Cancel</button>
+        </div> `,
+      size: 'sm'
+    }).result.then(() => this.window.location = 'http://documentation.bonitasoft.com/version/' + versionFragment);
   }
 }
