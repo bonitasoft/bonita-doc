44e0c381bd137f2137106a2b504e71c75d76bc61
redboul <julien.reboul@bonitasoft.com>
2016-05-02 11:54:38 +0200

    refactor html conversion script

--- site/scripts/convertMd2Html.js
+++ site/scripts/convertMd2Html.js
@@ -3,62 +3,44 @@
 (() => {
   'use strict';
 
-  let MarkdownIt = require('markdown-it');
-  let fs = require('fs');
+  require('es6-promise').polyfill();
 
+  let MarkdownIt = require('markdown-it');
+  let denodeify = require('denodeify');
+  let fs = require('fs');
+  
+  let readdirPromise = denodeify(fs.readdir);
+  let statPromise = denodeify(fs.stat);
+  let writeFilePromise = denodeify(fs.writeFile);
+  let accessPromise = denodeify(fs.access);
   let pathToMd = '../md/';
   let pathToHtml = 'app/html/';
 
-  fs.readdir(pathToMd, (err, dirs) => {
-    if (err) {
-      throw err;
-    }
+  readdirPromise(pathToMd).then(dirs => {
+    console.log(dirs);
     dirs.forEach(dir => {
       convertDirectory(pathToMd, pathToHtml, dir);
     });
-  });
+  }).catch(e => console.log(e));
   function convertDirectory(mdPath, htmlPath, dir) {
     console.log('converting ', mdPath, htmlPath, dir);
-    fs.stat(mdPath + dir, (err, stats) => {
-      if (err) {
-        throw err;
-      }
-      if (stats.isDirectory()) {
-        fs.readdir(mdPath + dir, (err, fileNames) => {
-          if (err) {
-            throw err;
-          }
-          try {
-            fs.accessSync(htmlPath + dir, fs.F_OK);
-          } catch (e) {
-            fs.mkdirSync(htmlPath + dir);
-          }
-          
-          fileNames.filter(fileName => fileName.match(/\.md$/)).forEach(fileName => {
-            let md = new MarkdownIt(); 
-            fs.writeFile(htmlPath + dir + '/' + fileName.replace(/\.md$/, '.html'), 
-               md.render(
-                 fs.readFileSync(mdPath + dir + '/' + fileName).toString()
-               ),
-               err => {
-                 if (err) {
-                   throw err;
-                 }
-                console.log(fileName + ' has been successfully converted');
-              });
-          });
-          fileNames.forEach(file => {
-            fs.stat(mdPath + dir + '/' + file, (err, stats) => {
-              if (err) {
-                throw err;
-              }
-              if (stats.isDirectory()) {
-                convertDirectory(mdPath + dir + '/', htmlPath + dir + '/', file);
-              }
-            });
-          });
-        });
-      }
-    });
+    statPromise(mdPath + dir)
+      .then(stats => stats.isDirectory() && readdirPromise(mdPath + dir)
+        .then(fileNames => accessPromise(htmlPath + dir, fs.F_OK)
+          .catch(e => fs.mkdirSync(htmlPath + dir))
+          .then(() => fileNames.filter(fileName => fileName.match(/\.md$/)).forEach(fileName => convertFile(dir, fileName, htmlPath, mdPath)))
+          .then(() => fileNames.forEach(file => statPromise(mdPath + dir + '/' + file)
+              .then(stats => stats.isDirectory() && convertDirectory(mdPath + dir + '/', htmlPath + dir + '/', file))
+            )
+          )
+        )
+    );
+  }
+  function convertFile(dir, fileName, htmlPath, mdPath) {
+    let md = new MarkdownIt(); 
+    writeFilePromise(htmlPath + dir + '/' + fileName.replace(/\.md$/, '.html'), 
+       md.render(
+         fs.readFileSync(mdPath + dir + '/' + fileName).toString()
+       )).then(() => console.log(fileName + ' has been successfully converted'));
   }
 })();
