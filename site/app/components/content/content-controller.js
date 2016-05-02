d774cd3e9a38d6bfbeadc5818d55cb32440e7967
Julien Reboul <julien.reboul@bonitasoft.com>
2016-05-02 23:23:43 +0200

    enable navigation from content to another page

--- site/app/components/content/content-controller.js
+++ site/app/components/content/content-controller.js
@@ -1,4 +1,5 @@
-import angular from 'angular';
+import $ from 'jquery';
+import _ from 'lodash';
 
 export default class ContentController {
   constructor($uiViewScroll, $stateParams, $state, currentVersion) {
@@ -11,20 +12,22 @@ export default class ContentController {
   goto($event) {
     if ($event.target.localName.match(/a/)) {
       if ($event.target.hash.match(/^[#]/)) {
-        this.$uiViewScroll(angular.element($event.target.hash));
+        this.$uiViewScroll($($event.target.hash));
         $event.stopPropagation();
         $event.preventDefault();
         return false;
       }
       console.log($event.target.attributes.href);
-      if ($event.target.attributes && $event.target.attributes.href && $event.target.attributes.href.value && $event.target.attributes.href.value.match(/^\//)) {
-        this.goToPage($event.target.attributes.href.value.replace(/^\/(.*)\.md$/, '$1'));
+      if ($event.target.attributes && $event.target.attributes.href && $event.target.attributes.href.value && !$event.target.attributes.href.value.match(/^http/)) {
+        this.goToPage($event.target.attributes.href.value.replace(/.md/, ''));
+        $event.preventDefault();
+        return false;
       }
     }
   }
   goToPage(page) {
-    this.$state.transitionTo(this.$state.current, angular.extend(this.$stateParams, { page: page }), {
-      reload: true, inherit: false, notify: true
+    this.$state.transitionTo(this.$state.current, _.extend(this.$stateParams, { page: page }), {
+      reload: false, inherit: false, notify: false
     });
   }
   getPage() {
