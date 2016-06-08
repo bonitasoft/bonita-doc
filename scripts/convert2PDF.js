2cf4322c284863f26a98f0a11c4bbc87de6eb9bd
redboul <julien.reboul@bonitasoft.com>
2016-06-08 14:27:42 +0200

    tr(pdf): include images to generation

--- scripts/convert2PDF.js
+++ scripts/convert2PDF.js
@@ -6,19 +6,60 @@
   require('es6-promise').polyfill();
 
   const denodeify = require('denodeify');
-  let fs = require('fs');
-  let winston = require('winston');
-  let markdownpdf = require('markdown-pdf');
+  const fs = require('fs');
+  const winston = require('winston');
+  const through = require('through');
+  const markdownpdf = require('markdown-pdf');
+  const html5pdf = require("html5-to-pdf");
+  const argv = require('yargs').argv;
+  const variables = require('./variables.json');
+  const taxonomy = require('../build/html/taxonomy.json');
+  let version = argv.v || argv._[0];
+   if (!version) {
+     version = variables.varVersion;
+     if (!version) {
+       winston.warn('usage convert2PDF [-v] version or set a "varVersion" key in scripts/variables.json');
+       return;
+     }
+   }
 
   const readdirPromise = denodeify(fs.readdir);
   //TODO have image be converted
-  let outfile = `build/Bonita-BPM-documentation.pdf`;
-  let mdDocs = ["md/taxonomy.md"];
-   
-  readdirPromise('md').then(fileNames => {
-    [].push.apply(mdDocs, fileNames.filter(fileName => fileName !== 'taxonomy.md').filter(fileName => !fileName.match(/^_/)).filter(fileName => fileName.match(/md$/)).map(fileName => 'md/' + fileName));
-    markdownpdf().concat.from(mdDocs).to(outfile, () => winston.info('documentation successfully generated to ' + outfile));
+  let outfile = `Bonita-BPM-documentation.pdf`;
+  let mdDocs = ['build/html/multi-language-pages.html'];//'build/html/taxonomy.html'];
+  const flattenedTaxo = flattenTaxonomy(taxonomy);
+  winston.info(flattenedTaxo);
+  let htmlFile = '';  
+
+  flattenedTaxo.forEach(fileName => htmlFile += fs.readFileSync('build/html/'+fileName).toString());
+
+  html5pdf({template:'htmlbootstrap', preProcessHtml: preProcessHtml}).from(htmlFile).to('build/'+outfile, function () {
+    console.log("Done");
   });
+
+
+  function flattenTaxonomy(node) {
+    return node.reduce((acc, curr) => {
+      if (curr.children) {
+        [].push.apply(acc, flattenTaxonomy(curr.children));
+      } else {
+        acc.push(curr.href);
+      }
+      return acc;
+    }, []);
+  }
+
+  readdirPromise('md').then(fileNames => {
+//    [].push.apply(mdDocs, fileNames.filter(fileName => fileName !== 'taxonomy.md').filter(fileName => !fileName.match(/^_/)).filter(fileName => fileName.match(/md$/)).map(fileName => 'md/' + fileName));
+ //   markdownpdf().concat.from(mdDocs).to(outfile, () => winston.info('documentation successfully generated to ' + outfile));
+    
+  }).then();
+  
+  function preProcessHtml() {
+    return through(function (data) {
+      return this.queue(data.toString().replace(/<a [^>]*>/g,'').replace(/<\/a\s*>/g,'').replace(new RegExp(`src="images/${version}`,'g'), `src="file://${__dirname}/../md/images/`));
+    });
+  }
   
 })();
-                
+               
