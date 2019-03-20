4c81ff07b27c4c873ff7f384a36595cc94142a22
julienmege <julien.mege@bonitasoft.com>
2019-03-20 16:00:47 +0100

    Revert "feat(code highlight) Add highlight.js"
    This reverts commit 4f135b7a5a0f5d641bffc3a75c5ac11cbf17bf69.

--- scripts/convertMdToHtml.js
+++ scripts/convertMdToHtml.js
@@ -40,19 +40,7 @@
   const decorate = require('markdown-it-decorate');
   const mdInline = require('markdown-it-for-inline');
   const alerts = require('markdown-it-alerts');
-  const hljs = require('highlight.js');
-  const md  = require('markdown-it')(
-      { html: true,
-        highlight: function (str, lang) {
-            if (lang && hljs.getLanguage(lang)) {
-                try {
-                    return hljs.highlight(lang, str).value;
-                } catch (__) {}
-            }
-
-            return ''; // use external default escaping
-        }
-      })
+  const md  = require('markdown-it')({ html: true })
     .use(fa)
     .use(decorate)
     .use(smartArrows)
