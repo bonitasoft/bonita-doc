4f135b7a5a0f5d641bffc3a75c5ac11cbf17bf69
julienmege <julien.mege@bonitasoft.com>
2019-03-20 10:17:03 +0100

    feat(code highlight) Add highlight.js

--- scripts/convertMdToHtml.js
+++ scripts/convertMdToHtml.js
@@ -40,7 +40,19 @@
   const decorate = require('markdown-it-decorate');
   const mdInline = require('markdown-it-for-inline');
   const alerts = require('markdown-it-alerts');
-  const md  = require('markdown-it')({ html: true })
+  const hljs = require('highlight.js');
+  const md  = require('markdown-it')(
+      { html: true,
+        highlight: function (str, lang) {
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return hljs.highlight(lang, str).value;
+                } catch (__) {}
+            }
+
+            return ''; // use external default escaping
+        }
+      })
     .use(fa)
     .use(decorate)
     .use(smartArrows)
